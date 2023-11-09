2220f0d8c522ef48c2ed4d0e148923469aaa43f1
lagillenwater <lagillenwater@gmail.com>
2023-11-09 10:16:52 -0700

    added gseaFinder function

--- R/partial_correlation_functions.R
+++ R/partial_correlation_functions.R
@@ -22,3 +22,42 @@ multiMediators <- function(input, pathways) {
   print(head(input,20))
 } 
 
+## gseaFinder is a helper function for filtering the gsea results.
+## Inputs: character cyt-met example
+##         charcter key pathway to search
+## Outputs: vector of adjusted p value, NES, leading edge genes
+
+gseaFinder <- function(cyt_met_example, key_pathway) {
+
+  cyt_met_labels <- names(gsea_results_formatted[[1]]) 
+  cyt_met_labels <- cyt_met_labels[grepl("NES", cyt_met_labels)]
+  cyt_met_labels <- gsub(" NES", "", cyt_met_labels)
+  path_id <- which(cyt_met_example ==cyt_met_labels)
+  
+  tmp_gsea <- lapply(gsea_results, function(x)
+    x[[path_id]])
+  
+  res <- lapply(tmp_gsea, function(x) {
+    x %>% filter(pathway == key_pathway)
+  })
+  
+ final_res <- bind_rows(res) %>% dplyr::select(c(padj,NES,leadingEdge)) %>% as.data.frame()
+  final_res <- final_res[1,]
+return(final_res)
+  
+}
+
+tmp <- gseaFinder("IFN-gamma-kynurenine", "HALLMARK_INTERFERON_GAMMA_RESPONSE")
+tmp$leadingEdge
+gseaFinder("IFN-gamma-kynurenine", "HALLMARK_HEME_METABOLISM")
+gseaFinder("IL-1RA-Glutamate", "HALLMARK_INTERFERON_GAMMA_RESPONSE")
+tmp3 <- gseaFinder("IL-1RA-Glutamate", "HALLMARK_HEME_METABOLISM")
+tmp3$leadingEdge
+
+tmp2 <- gseaFinder("IFN-gamma-acyl-C14:1 (Tetradecenoyl Carnitine)", "HALLMARK_INTERFERON_GAMMA_RESPONSE")
+tmp2$leadingEdge
+
+gseaFinder("IL-29-Docosapentaenoic acid", "HALLMARK_HEME_METABOLISM")
+
+
+
