0cd18e194e00be7f5eefc6f16a2b142176c128b9
lagillenwater <lagillenwater@gmail.com>
2023-11-16 11:12:29 -0700

    function for combing deltaZ and DEG

--- docs/figures/005_figure5.R
+++ docs/figures/005_figure5.R
@@ -21,27 +21,31 @@ diffZ <- function(z_scores_list, cluster1, cluster2) {
 }
 
 
-## corrZandDEG is a function for finding the correlation between difference in Z scores between karyotypes/clusters and DEG by karyotypes/clusters for a specific cytokine_metabolite relationship
+### NB: For comparison, need a function that finds difference in Z score for 1 cluster vs all other clusters
 
-corrZandDEG <- function(key_cyt_met, 
-key_cyt_met <- "IFN-gamma-kynurenine"
+## ZandDEG is a function for combining the change in Z scores between karyotypes/clusters and DEG by karyotypes/clusters for a specific cytokine_metabolite relationship
 
-tmp_diff <- diff_genes[[1]] %>%
-    rownames_to_column("gene")
+ZandDEG <- function(key_cyt_met, delta_z, DEG) {
+
+    tmp_diff <- DEG %>%
+        rownames_to_column("gene")
+
+    diff_and_z <- delta_z  %>%
+        as.data.frame() %>%
+        select(key_cyt_met) %>%
+        rownames_to_column("gene")%>%
+        left_join(tmp_diff, by = "gene") 
+
+    return(diff_and_z)
+}
 
 
-
-
-
-
-diff_and_z <- long_z[[1]] %>%
-    filter(cyt_met == key_cyt_met) %>%
-    left_join(tmp_diff, by = "gene") 
-
+plotZandDEG <- function(z_and_DEG) {
+    
 
 ### Figure 5X
 diff_T21_D21 <- diffZ(full_z_results, cluster1 = "T21", cluster2 = "D21")
-
+Z_DEG_T21_D21 <- ZandDEG(key_cyt_met = "IFN-gamma-kynurenine",DEG =diff_genes[[1]],delta_z =  diff_T21_D21)
 
 
 ggplot(diff_and_z, aes(x = combined_Z, y = logFC)) +
